454ff163d7d2869ffc0624319e914db640f2eda5
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-10-20 02:19:01 +0200

    Initial Flask app setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from app import views
+app = Flask(__name__)
+
+#url
+app.add_url_rule('/','base',views.base)
+
+
+
+#run
+if __name__ == "__main__":
+    app.run(debug=True)
