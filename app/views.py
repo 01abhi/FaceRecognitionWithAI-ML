f69fbc73a0e01c3cb60ae452053fada9746f7d46
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-10-22 23:40:14 +0200

    Added view and form to upload and predict

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from flask import render_template, request
 from flask import redirect, url_for
+import os
+
+UPLOADS_FOLEDER = 'static/uploads'
+
 
 def base():
     return render_template("base.html")
