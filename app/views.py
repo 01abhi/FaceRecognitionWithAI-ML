033c262143140dbdc3f178297f77e5b599fd414a
Abhijeet Manny <abhijeetmanny@gmail.com>
2021-10-23 01:57:02 +0200

    Fixed aspect ratio according to image uploaded

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from flask import redirect, url_for
 import os
+from PIL import Image
 
 UPLOADS_FOLDER = 'static/uploads'
 
@@ -20,5 +21,13 @@ def gender():
         filename = f.filename
         path = os.path.join(UPLOADS_FOLDER,filename)
         f.save(path)
-        return render_template("gender.html", fileupload = True, img_name= filename)
-    return render_template("gender.html", fileupload = False, img_name = "FacerecAI.png")
+        w = getwidth(path)
+        return render_template("gender.html", fileupload = True, img_name= filename, w = w)
+    return render_template("gender.html", fileupload = False, img_name = "FacerecAI.png", w = 300)
+
+def getwidth(path):
+    img = Image.open(path)
+    size = img.size
+    aspect = size[0]/size[1] #aspect ratio
+    w = 300 * aspect
+    return int(w)
